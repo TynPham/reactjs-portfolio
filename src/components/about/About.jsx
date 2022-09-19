330426595adf3ff6b616afd6695b5b2a990d06bb
pham tuyen <phamtuyendz1104@gmai.com>
2022-09-20 00:04:04 +0700

    make about and experience

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,55 @@
 import React from "react";
 import "./About.scss";
+import ME from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { HiUsers } from "react-icons/hi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5 className="text-light">Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container container-about">
+        <div className="me">
+          <div className="me-bg">
+            <img src={ME} alt="" className="me-image" />
+          </div>
+        </div>
+
+        <div className="content">
+          <div className="content-cards">
+            <article className="card">
+              <FaAward className="icon" />
+              <h5>Experience</h5>
+              <small className="text-light">3+ Years Working</small>
+            </article>
+            <article className="card">
+              <HiUsers className="icon" />
+              <h5>Clients</h5>
+              <small className="text-light">200+ Workwide</small>
+            </article>
+            <article className="card">
+              <VscFolderLibrary className="icon" />
+              <h5>Projects</h5>
+              <small className="text-light">80+ Completed</small>
+            </article>
+          </div>
+
+          <p className="text-light">
+            Lorem ipsum, dolor sit amet consectetur adipisicing edit. Lorem
+            ipsum, dolor sit amet consectetur adipisicing edit. Lorem ipsum,
+            dolor sit amet consectetur adipisicing edit. Lorem ipsum, dolor sit
+            amet consectetur adipisicing edit.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,8 +1,104 @@
 import React from "react";
 import "./Experience.scss";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container container-experience">
+        <div className="dev front-end">
+          <h3>Frontend Developer</h3>
+          <div className="content">
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">HTML</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">CSS</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">Javascript</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">Bootstrap</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">Tailwind</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">React</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="dev back-end">
+          <h3>Backend Developer</h3>
+          <div className="content">
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">Node Js</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">MongoDB</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">PHP</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">My SQL</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+            <article className="details">
+              <BsPatchCheckFill className="icon" />
+              <div>
+                <h4 className="skill">Python</h4>
+                <small className="level">Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
